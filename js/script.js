4c8aa12c346c7da7c6479b510c2cb879858bbda8
ChristineWasike <christinewasike22@gmail.com>
2017-06-01 07:06:41 +0300

    added html file js and css folder

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var sentence=prompt("What is your favourite sentence?");
+var all = function() {
+	var letter1 = sentence[0];
+	var letter2 = sentence[sentence.length -1];
+	var join = letter1 + letter2;
+	var capital = join.toUpperCase();
+	return capital
+}
+
+var rewind = function() {
+	var letter1 = all()[0];
+	var letter2 = all()[all().length -1];
+	var combine = letter1 + letter2;
+	return combine;
+}
+
+function reverseString(str){
+	var splitString = str.split("");
+	var reverseArray = splitString.reverse();
+	var reversedString = reverseArray.join("");
+	return reversedString;
+}
+
+var answer = reverseString(rewind());
+console.log(answer);
+
+alert(rewind());
+console.log(rewind())
+
+var newSentence = function() {
+	var code = sentence + rewind();
+	return code;
+}
+
+alert(newSentence());
+
+var integer = function() {
+	var numChar = sentence.length-1 /2;
+	var position = sentence.charAt(numChar);
+	var ended = position + sentence;
+}
+var final = integer(sentence);
+alert(final);
